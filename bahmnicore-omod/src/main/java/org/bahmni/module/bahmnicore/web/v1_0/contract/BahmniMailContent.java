a4ee32febd997c30ca3d927e19b3917e7a66e3df
Kavitha S <kavitha.s@thoughtworks.com>
2023-02-22 17:37:17 +0530

    BAH-2189 | refactored contract and url mapping

--- bahmnicore-omod/src/main/java/org/bahmni/module/bahmnicore/web/v1_0/contract/BahmniMailContent.java
+++ bahmnicore-omod/src/main/java/org/bahmni/module/bahmnicore/web/v1_0/contract/BahmniMailContent.java
@@ -11,9 +11,9 @@ import lombok.Setter;
 @NoArgsConstructor
 public class BahmniMailContent {
     private String pdf;
+    private String fileName;
     private String subject;
     private String body;
-    private BahmniRecipient recipient;
     private String[] cc;
     private String[] bcc;
 }
--- bahmnicore-omod/src/main/java/org/bahmni/module/bahmnicore/web/v1_0/controller/TransmissionController.java
+++ bahmnicore-omod/src/main/java/org/bahmni/module/bahmnicore/web/v1_0/controller/TransmissionController.java
@@ -11,7 +11,6 @@ import org.apache.http.message.BasicStatusLine;
 import org.bahmni.module.bahmnicore.web.v1_0.contract.BahmniMailContent;
 import org.bahmni.module.communication.api.CommunicationService;
 import org.bahmni.module.communication.model.MailContent;
-import org.bahmni.module.communication.model.Recipient;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.springframework.stereotype.Controller;
@@ -19,22 +18,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import javax.mail.MessagingException;
 
 @Controller
-@RequestMapping(value = "/rest/" + RestConstants.VERSION_1 + "/send/")
+@RequestMapping(value = "/rest/" + RestConstants.VERSION_1 + "patient/{patientUuid}/send/")
 public class TransmissionController {
 
     private final Log log = LogFactory.getLog(this.getClass());
 
     @PostMapping(value = "email")
     @ResponseBody
-    public Object sendEmail(@RequestBody BahmniMailContent bahmniMailContent) {
+    public Object sendEmail(@RequestBody BahmniMailContent bahmniMailContent, @PathVariable("patientUuid") String patientUuid) {
         HttpResponseFactory factory = new DefaultHttpResponseFactory();
         HttpResponse response = null;
         try {
             MailContent mailContent = transformBahmniMailContent(bahmniMailContent);
-            Context.getService(CommunicationService.class).sendEmail(mailContent);
+            Context.getService(CommunicationService.class).sendEmail(mailContent, patientUuid);
             response = factory.newHttpResponse(new BasicStatusLine(HttpVersion.HTTP_1_1, HttpStatus.SC_OK, null), null);
         } catch (Exception exception) {
             log.error("Unable to send email", exception);
@@ -44,7 +44,6 @@ public class TransmissionController {
     }
 
     private MailContent transformBahmniMailContent(BahmniMailContent bahmniMailContent) {
-        Recipient recipient = new Recipient(bahmniMailContent.getRecipient().getName(),bahmniMailContent.getRecipient().getEmail());
-        return new MailContent(bahmniMailContent.getPdf(), bahmniMailContent.getSubject(),bahmniMailContent.getBody(), recipient, bahmniMailContent.getCc(), bahmniMailContent.getBcc());
+        return new MailContent(bahmniMailContent.getPdf(), bahmniMailContent.getFileName(), bahmniMailContent.getSubject(),bahmniMailContent.getBody(), bahmniMailContent.getCc(), bahmniMailContent.getBcc());
     }
 }
