17471dd36b96abf62da0e3528ce97fbb26cc3d9c
MOHANKUMAR T <mohan13081999@gmail.com>
2024-02-03 12:18:00 +0530

    BAH-3513 | Add. Message for the condition block added

--- bahmnicore-api/src/main/java/org/bahmni/module/bahmnicore/forms2/service/impl/BahmniFormDetailsServiceImpl.java
+++ bahmnicore-api/src/main/java/org/bahmni/module/bahmnicore/forms2/service/impl/BahmniFormDetailsServiceImpl.java
@@ -58,6 +58,8 @@ public class BahmniFormDetailsServiceImpl implements BahmniFormDetailsService {
         Patient patient = getPatient(patientUuid);
         List<Visit> visits = visitService.getVisitsByPatient(patient);
 
+        //Warning: This check is needed to avoid getEncounters returning ALL non-voided encounters of all patients leading to OutOfMemoryError:Java Heap
+        //Refer: https://bahmni.atlassian.net/browse/BAH-3513
         if(visits.isEmpty())
             return Collections.emptyList();
 
