64c92d3ac9eed58182b426fd2335a02bd80cd2ad
Aishwarya-thoughtworks <aishwarya.sontakke@thoughtworks.com>
2026-05-04 01:17:38 +0530

    BAH-4222| Update SQL query with location filtering, activeVisitUuid, and voided orders check

--- bahmnicore-omod/src/main/resources/V1_100_AddActivePatientWithLabOrdersQuery.sql
+++ bahmnicore-omod/src/main/resources/V1_100_AddActivePatientWithLabOrdersQuery.sql
@@ -6,14 +6,16 @@ VALUES ('emrapi.sqlSearch.activePatientsWithLabOrders',
         'select distinct
           concat(pn.given_name, " ", pn.family_name) as name,
           pi.identifier as identifier,
-          concat("",p.uuid) as uuid
+          concat("",p.uuid) as uuid,
+          concat("",v.uuid) as activeVisitUuid
         from visit v
         join person_name pn on v.patient_id = pn.person_id and pn.voided = 0
         join patient_identifier pi on v.patient_id = pi.patient_id
         join patient_identifier_type pit on pi.identifier_type = pit.patient_identifier_type_id
         join global_property gp on gp.property="bahmni.primaryIdentifierType" and gp.property_value=pit.uuid
         join person p on p.person_id = v.patient_id
-        join orders on orders.patient_id = v.patient_id
+        join location l on l.uuid = ${location_uuid} and v.location_id = l.location_id
+        join orders on orders.patient_id = v.patient_id and orders.voided = 0
         join order_type on orders.order_type_id = order_type.order_type_id and order_type.name = "Lab Order"
         where v.date_stopped is null AND v.voided = 0 and order_id not in
           (select obs.order_id
